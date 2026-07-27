3dfa17a3dd9c4a1eac443df73f72e305211e3617
Owolabi Victor <vowolabi16@gmail.com>
2026-07-27 19:55:49 +0100

    Fixed navigation bug

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const hamburgerMenu = document.querySelector(".fa-bars")
 const navLists = document.querySelector(".nav-lists")
 
 hamburgerMenu.addEventListener("click", () => {
-    navLists.style.display = "flex"
+    navLists.style.left = "0"
 })
 
 navLists.addEventListener("click", (e) => {
-    if(e.target.classList.contains("nav-links") || e.target.classList.contains("fa-xmark")){
-        navLists.style.display = "none"
+    if(e.target.classList.contains("nav-links") || e.target.classList.contains("fa-arrow-right-from-bracket")){
+        navLists.style.left = "-100%"
     }
 })
